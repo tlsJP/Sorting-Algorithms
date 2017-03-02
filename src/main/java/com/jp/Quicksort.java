26ad2fe2a17b198e2f2de9850bcad9462c0bbd84
John Albert <jp@thejpaproject.com>
2017-03-01 23:42:14 -0800

    insertion sort completed

---
+++ src/main/java/com/jp/Quicksort.java
@@ -0,0 +1,11 @@
+package com.jp;
+
+/**
+ * Created by JP on 3/1/2017.
+ */
+public class Quicksort implements SortingAlgorithm {
+    @Override
+    public int[] sort(int[] unsortedArray) {
+        return new int[0];
+    }
+}
